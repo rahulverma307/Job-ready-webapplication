62d69a067c006f5efd5fc4beff014dd6a1034697
Rahul Verma <rahulsi9810@gmail.com>
2026-06-19 22:21:27 +0530

    feat: add interview report model and remove architecture notes file

---
+++ Backend/src/models/interbiewReport.model.js
@@ -0,0 +1,18 @@
+const mongoose= require("mongoose");
+
+/***
+ * 
+ * Job description schema
+ * resume text
+ * self desription
+ * technical question:[{question:"",answer:""}]
+ * behavioral question:[{question:"",answer:""}]
+ * skill based question:[{question:"",answer:""}]
+ * feedback:[{question:"",answer:""}]
+ * preparation plane:[{question:"",answer:""}]
+ * 
+ */
+
+const interviewReportSchema=new mongoose.Schema({
+    
+})
